b7f44f86db7eb335f4880939bb9ac3565d9125be
Alberto Romero <albertorh83@gmail.com>
2015-07-21 12:18:09 +0200

    Se crea el proyecto de Dominio y las entidades de negocio

---
+++ SignalRAPI/SignalRAPI.Domain/CoursesAdministration/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRAPI.Domain.CoursesAdministration
+{
+    public class Course
+    {
+        [Key]
+        public Guid CourseId { get; set; }
+
+        [StringLength(50, MinimumLength = 1)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+
+    }
+}
---
+++ SignalRAPI/SignalRAPI.Domain/CoursesAdministration/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRAPI.Domain.CoursesAdministration
+{
+    public class Student
+    {
+        [Key]
+        public Guid StudentId { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 1)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 1)]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        public string Alias { get; set; }
+
+        public Guid CourseId { get; set; }
+
+        public virtual Course Course { get; set; }
+
+    }
+
+}
