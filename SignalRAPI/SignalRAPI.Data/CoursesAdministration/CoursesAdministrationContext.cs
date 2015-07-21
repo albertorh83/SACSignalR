86a2331e44ea4b46b04de228b777e356e7e61c8d
Alberto Romero <albertorh83@gmail.com>
2015-07-21 12:27:25 +0200

    Contexto

---
+++ SignalRAPI/SignalRAPI.Data/CoursesAdministration/CoursesAdministrationContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalRAPI.Domain.CoursesAdministration;
+
+namespace SignalRAPI.Data.CoursesAdministration
+{
+    public class CoursesAdministrationContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
