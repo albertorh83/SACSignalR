dc0e56cf77fc81f0896331db99a190ff5d79816f
Alberto Romero <albertorh83@gmail.com>
2015-07-21 12:59:39 +0200

    Entity repositories

---
+++ SignalRAPI/SignalRAPI.Data/Repositories/CoursesRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalRAPI.Data.Repository;
+using SignalRAPI.Data.UnitOfWorks;
+using SignalRAPI.Domain.CoursesAdministration;
+
+namespace SignalRAPI.Data.Repositories
+{
+    public class CoursesRepository : DomainRepository<Course>
+    {
+        public CoursesRepository( CoursesUnitOfWork coursesUnitOfWork)
+            : base(coursesUnitOfWork)
+        {
+        }
+    }
+
+}
---
+++ SignalRAPI/SignalRAPI.Data/Repositories/StudentsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalRAPI.Data.Repository;
+using SignalRAPI.Data.UnitOfWorks;
+using SignalRAPI.Domain.CoursesAdministration;
+
+namespace SignalRAPI.Data.Repositories
+{
+    public class StudentsRepository : DomainRepository<Student>
+    {
+        public StudentsRepository(CoursesUnitOfWork coursesUnitOfWork)
+            : base(coursesUnitOfWork)
+        {
+        }
+    }
+
+}
