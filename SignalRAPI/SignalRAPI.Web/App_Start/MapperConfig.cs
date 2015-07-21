025facf03f9cdb21224a75dfdc9b58e4b6779d60
Alberto Romero <albertorh83@gmail.com>
2015-07-21 13:56:35 +0200

    Configuring automapper

---
+++ SignalRAPI/SignalRAPI.Web/App_Start/MapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using SignalR.Resources;
+using SignalRAPI.Domain.CoursesAdministration;
+
+namespace SignalRAPI.Web
+{
+    public static class MapperConfig
+    {
+        public static void ConfigMaps()
+        {
+
+            Mapper.CreateMap<Course, CourseResource>();
+            Mapper.CreateMap<Student, StudentResource>();
+
+        }
+    }
+}
